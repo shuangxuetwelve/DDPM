b12ac58a27dc9704d0c43724bf0e17eb28953d39
shuangxuetwelve <shuangxue12@outlook.com>
2023-03-30 11:01:04 +0800

    Use diffusion model on FashionMNIST

--- utils.py
+++ utils.py
@@ -24,3 +24,27 @@ class MNIST(torchvision.datasets.MNIST):
         else:
             self.cache[idx] = super().__getitem__(idx)
             return self.cache[idx]
+        
+class FashionMNIST(torchvision.datasets.FashionMNIST):
+
+    cache = {}
+
+    def __init__(self, path, train=True, transform=None):
+
+        super().__init__(path, download=True, train=train, transform=transform)
+
+        # Access all samples to pre-cache them.
+        for idx in range(self.__len__()):
+            self.__getitem__(idx)
+
+    def __len__(self):
+
+        return super().__len__()
+    
+    def __getitem__(self, idx):
+
+        if idx in self.cache:
+            return self.cache[idx]
+        else:
+            self.cache[idx] = super().__getitem__(idx)
+            return self.cache[idx]
