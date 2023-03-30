fde660c92922ae0b74173e3acd0d648a51e0517f
shuangxuetwelve <shuangxue12@outlook.com>
2023-03-30 17:07:54 +0800

    Apply diffusion model to CelebA

--- utils.py
+++ utils.py
@@ -72,3 +72,27 @@ class CIFAR10(torchvision.datasets.CIFAR10):
         else:
             self.cache[idx] = super().__getitem__(idx)
             return self.cache[idx]
+        
+class CelebA(torchvision.datasets.CelebA):
+
+    cache = {}
+
+    def __init__(self, path, split='train', transform=None):
+
+        super().__init__(path, download=True, split=split, transform=transform)
+
+        # Access all samples to pre-cache them.
+        for idx in range(self.__len__()):
+            self.__getitem__(idx)
+
+    def __len__(self):
+
+        return super().__len__()
+    
+    def __getitem__(self, idx):
+
+        if idx in self.cache:
+            return self.cache[idx]
+        else:
+            self.cache[idx] = super().__getitem__(idx)
+            return self.cache[idx]
