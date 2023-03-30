da61551ca1095fe8debbcbbbde49b0ead77bd2a4
shuangxuetwelve <shuangxue12@outlook.com>
2023-03-30 11:56:08 +0800

    Apply diffusion model on CIFAR10

--- utils.py
+++ utils.py
@@ -48,3 +48,27 @@ class FashionMNIST(torchvision.datasets.FashionMNIST):
         else:
             self.cache[idx] = super().__getitem__(idx)
             return self.cache[idx]
+        
+class CIFAR10(torchvision.datasets.CIFAR10):
+
+    cache = {}
+
+    def __init__(self, path, train=True, transform=None):
+
+        super().__init__(path, download=True, train=train, transform=transform)
+
+        # Access all samples to pre-cache them.
+        for idx in range(self.__len__()):
+            self.__getitem__(idx)
+
+    def __len__(self):
+
+        return super().__len__()
+    
+    def __getitem__(self, idx):
+
+        if idx in self.cache:
+            return self.cache[idx]
+        else:
+            self.cache[idx] = super().__getitem__(idx)
+            return self.cache[idx]
